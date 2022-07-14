a5bff32a9f4647f2bcbb200cb21af9bedd7fb215
InakiGainza <igg.git.h@gmail.com>
2022-07-14 12:27:34 +0200

    commit "move other css files to folder other-css"

---
+++ config/php_errors_config.php
@@ -0,0 +1,13 @@
+<?php
+
+# 
+#  php errors to log file php_errors.log in asstes folder
+# 
+$php_errors_log = $_SERVER['DOCUMENT_ROOT'] . "/lanbide2022/gaztelu/curso-backend-areafor-server/assets/log-files/php_errors.log";
+error_reporting(E_ALL);
+ini_set('ignore_repeated_errors',TRUE);
+ini_set('display_errors', FALSE);
+ini_set("log_errors",TRUE);
+ini_set('error_log',$php_errors_log);
+
+?>
