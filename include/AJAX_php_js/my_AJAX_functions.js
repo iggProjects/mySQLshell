08f01d5a3c8665512a7560a16dd9ae4de8d06836
iggProjects <inaki.gainzarain@gmail.com>
2022-07-15 18:51:13 +0200

    COMMIT "JS"

--- include/AJAX_php_js/my_AJAX_functions.js
+++ include/AJAX_php_js/my_AJAX_functions.js
@@ -7,6 +7,8 @@
 */
 
 
+
+
 function Display_div_nav_izq(tag,host_array,php_sql_url){
 
     console.log('tag=> ' + tag + '\nphp_sql_url=> ' + php_sql_url) 
@@ -36,50 +38,47 @@ function Display_div_nav_izq(tag,host_array,php_sql_url){
 }
 
     // view Tree UL
-    function openTree(host_arr) {
+function openTree(host_arr) {
 
-        var toggler = document.getElementsByClassName("caret");
-        var i;
-        console.log('caret length ' + toggler.length);
+    var toggler = document.getElementsByClassName("caret");
+    var i;
+    console.log('caret length ' + toggler.length);
 
-        // Define listeners for "caret" class 
-        for (i = 0; i < toggler.length; i++) {
-            toggler[i].addEventListener("click", function() {
-                console.log('this class: ' + this.classList + ', tag: '+ this.tagName);
-                console.log('parent El: ' + this.parentElement.tagName  + ', class: '+ this.parentElement.classList);
-                this.parentElement.querySelector(".nested").classList.toggle("active");
-                this.classList.toggle("caret-down");
-            });
-        }
-
-        // Define listeners for "dispTbl" class, to display table structure (via sql query) 
-        var tableDisplay = document.getElementsByClassName("dispTbl");
-        var j;
-        // var host_server = <?php echo json_encode($host_serv); ?>;        
-
-        for (j = 0; j < tableDisplay.length; j++) {    
-            
-            tableDisplay[j].addEventListener("click", function() {    
-                
-                // Parameters for calling ajax junction
-                var point = this.getAttribute('point');
-                var hostNumb = this.getAttribute('host');
-                var tblName = this.getAttribute('table-name'); 
-                var dbName = this.getAttribute('db');
-                var _tag='display_sql_result';
-               
-                // call ajax table function
-                ShowTblCols_Left_Aside_js(_tag,'./include/AJAX_php_js/ajax_SelectTbl.php?hostName='+host_arr[hostNumb]+'&dbName='+dbName+'&tblName='+tblName);                    
-                // DescribeTbl_js(_tag,'./include/php_funct/ajax_SelectTbl.php?hostName='+host_arr[hostNumb]+'&dbName='+dbName+'&tblName='+tblName);                    
-                
-            });      
-      
-        }
-
+    // Define listeners for "caret" class 
+    for (i = 0; i < toggler.length; i++) {
+        toggler[i].addEventListener("click", function() {
+            console.log('this class: ' + this.classList + ', tag: '+ this.tagName);
+            console.log('parent El: ' + this.parentElement.tagName  + ', class: '+ this.parentElement.classList);
+            this.parentElement.querySelector(".nested").classList.toggle("active");
+            this.classList.toggle("caret-down");
+        });
     }
 
+    // Define listeners for "dispTbl" class, to display table structure (via sql query) 
+    var tableDisplay = document.getElementsByClassName("dispTbl");
+    var j;
+    // var host_server = <?php echo json_encode($host_serv); ?>;        
 
+    for (j = 0; j < tableDisplay.length; j++) {    
+        
+        tableDisplay[j].addEventListener("click", function() {    
+            
+            // Parameters for calling ajax junction
+            var point = this.getAttribute('point');
+            var hostNumb = this.getAttribute('host');
+            var tblName = this.getAttribute('table-name'); 
+            var dbName = this.getAttribute('db');
+            var _tag='display_sql_result';
+            
+            // call ajax table function
+            ShowTblCols_Left_Aside_js(_tag,'./include/AJAX_php_js/ajax_SelectTbl.php?hostName='+host_arr[hostNumb]+'&dbName='+dbName+'&tblName='+tblName);                    
+            // DescribeTbl_js(_tag,'./include/php_funct/ajax_SelectTbl.php?hostName='+host_arr[hostNumb]+'&dbName='+dbName+'&tblName='+tblName);                    
+            
+        });      
+    
+    }
 
+}
 
 
 /*
