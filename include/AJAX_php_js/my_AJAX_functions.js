40c3e577312c6f4b2f21f5df1842d11c874d2fa2
InakiGainza <igg.git.h@gmail.com>
2022-07-28 15:02:52 +0200

    commit "index & my_AJAX func"

--- include/AJAX_php_js/my_AJAX_functions.js
+++ include/AJAX_php_js/my_AJAX_functions.js
@@ -243,7 +243,7 @@ function Fetch_data_array(php_sql_url) {
         // FILLING RIGHT_TABLE        
         for ( i=index; i<data_array.length; i++ ) {
             console.log('data_array row: ' + i + ' -> ' + data_array[i]['TBL'] + ' - ' + data_array[i]['COL'] + ' - ' + data_array[i]['REF_COL'] + ' - ' + data_array[i]['REF_TBL']); 
-            if ( tbl_name == data_array[i]['TBL'] ) { right_table.push( data_array[i]['COL'] + ' - ' + data_array[i]['REF_TBL']) } 
+            if ( tbl_name == data_array[i]['TBL'] ) { right_table.push( data_array[i]['REF_COL'] + ' - ' + data_array[i]['REF_TBL']) } 
             else { index = i; }
         }
         console.log('index: ' + index);
@@ -258,23 +258,18 @@ function Fetch_data_array(php_sql_url) {
         var cv_h = document.getElementById("canvas").height;
 
         // console.log('canvas width: ' + cv_w + ' - height - ' + cv_h);
-        
-        // arrays for append de (x,y) coordinates of source and destiny points to make respective "relation lines"
+        draw_db(cv_w,cv_h,table_ppal,left_table,right_table);          
+           
+        // Drawing lines area
+        // arrays for append de (x,y) coordinates of source and destiny points to make respective "relation lines"                     
         var left_lines_x_y_array = [];
         var right_lines_x_y_array = [];     
-        
-        // for testing
+         
+        /*  Testing */
         //left_lines_x_y_array[0] = [{'x1':0,'y1':400},{'x2':300,'y2':400}];
         //left_lines_x_y_array[1] = [{'x1':0,'y1':450},{'x2':300,'y2':450}];
 
-        //right_lines_x_y_array[0] = [{'x1':600,'y1':300},{'x2':300,'y2':300}];
-        //right_lines_x_y_array[1] = [{'x1':600,'y1':500},{'x2':300,'y2':500}];   
-
-        draw_db(cv_w,cv_h,table_ppal,left_table,right_table);  
-        
-        /*  Testing */
-        //draw_table_relations_lines(left_lines_x_y_array);
-
+        // draw_table_relations_lines(left_lines_x_y_array);
 
     })    
 
@@ -287,11 +282,8 @@ function Fetch_data_array(php_sql_url) {
 }
 
 function draw_pair_points_x_y(pair_points) {
-    //alert(pair_points[0][0]['x1'] + ' (x1,y1) ' + pair_points[0][0]['y1']);
-    //alert(pair_points[0][1]['x2'] + ' (x2,y2) ' + pair_points[0][1]['y2']);
     var ctx = document.getElementById("canvas").getContext("2d");
     ctx.beginPath();
-
     /* testing */
     ctx.moveTo(pair_points[0][0]['x1'],pair_points[0][0]['y1']);
     ctx.lineTo(pair_points[0][1]['x2'],pair_points[0][1]['y2']);  
@@ -302,9 +294,12 @@ function draw_pair_points_x_y(pair_points) {
 
 /*  USED FOR "LEFT" AND "RIGHT" POINTS ARRAYS  */
 function draw_table_relations_lines(points_array) {
+
     //alert(JSON.stringify(points_array));  
     draw_pair_points_x_y(points_array);   
     /* HERE: loop through array elements  */  
+
+
 
 }      
 
@@ -313,14 +308,12 @@ function draw_table_row(_ctxx,_text,_xx,_yy,_w,_hh) {
     _ctxx.fillText(_text,_xx+(_w/2),_yy+(_hh/2));
 }
 
-function draw_table(_ctx,_table,_x,_y,_w,_h){
-    
+function draw_table(_ctx,_table,_x,_y,_w,_h){    
     _x = _x - _w/2;
     for ( var i=0; i < _table.length; i++) {  
         draw_table_row(_ctx,_table[i],_x,_y,_w,_h);
         _y += _h;
     }
-
 }
 
 function draw_db(w,h,ppal_table_array,left_tables_array,right_tables_array){
@@ -343,43 +336,123 @@ function draw_db(w,h,ppal_table_array,left_tables_array,right_tables_array){
 
     // parameters to draw ppal_table_array
     var x = w/2;
-    var y = 10; 
+    var y = 50; 
     
     // max length text of elements in table
     var max_large_text = 200;
-//    for ( var i=0; i < ppal_table_array.length; i++ ){  
-//        if ( max_large_text < ppal_table_array[i].length ) { max_large_text = ppal_table_array[i]; } 
-//    }
-    //var text_w = ctx.measureText(max_large_text).width * 1.4;
+
+/*    
+    for ( var i=0; i < ppal_table_array.length; i++ ){  
+        if ( max_large_text < ppal_table_array[i].length ) { max_large_text = ppal_table_array[i]; } 
+    }
+    ar text_w = ctx.measureText(max_large_text).width * 1.4;
+*/
+
     text_w = max_large_text;
     console.log('draw_db, ppal text_w: ' + text_w + ' | array length: ' + ppal_table_array.length);              
     
     draw_table(ctx,ppal_table_array,x,y,text_w,row_h);
 
     // parameters to draw left_table_array
-    x = 10;
-    max_large_text = 0;
+    x = 20;
+    max_large_text = 200;
+
+/*    
     text_w = 0;
     for ( var i=0; i < left_tables_array.length; i++ ){  
         if ( max_large_text < left_tables_array[i].length ) { max_large_text = left_tables_array[i]; } 
     }
-    text_w = ctx.measureText(max_large_text).width * 1.4;  
+    //text_w = ctx.measureText(max_large_text).width * 1.4;  
+*/
+
+    text_w = max_large_text;
     console.log('draw_db, left text_w: ' + text_w);              
-    x = 10 + text_w/2;             
-    y =  10;            
-    draw_table(ctx,left_tables_array,x,y,text_w,row_h);
+    x = 20 + text_w/2;             
+    y = 50;     
+    // Loop for differentiate distinct tables inside 'left_tables_array'
+    var temp_array = [];
+    var temp_tbl_name = left_tables_array[0].substr(0,left_tables_array[0].indexOf(" -"));
+    // console.log('table name init -> ' +  temp_tbl_name);
+    for ( var i=0; i<left_tables_array.length; i++ ) {
+
+        //console.log('step: ' + i);
+        //console.log('table name -> ' + left_tables_array[i].substr(0,left_tables_array[i].indexOf(" -")));
+        //console.log('table name -> ' + temp_tbl_name);
+        
+        if ( temp_tbl_name == left_tables_array[i].substr(0,left_tables_array[i].indexOf(" -")) ) { 
+            temp_array.push(left_tables_array[i]);            
+        }  
+        else { 
+            
+            //console.log('temp_array');
+            //console.log(temp_array);            
+            draw_table(ctx,temp_array,x,y,text_w,row_h);
+            y += 100;            
+            temp_array.length = 0;
+            temp_array.push(left_tables_array[i]); 
+            temp_tbl_name = left_tables_array[i].substr(0,left_tables_array[i].indexOf(" -"));   
+
+        }    
+
+    }
+    // temp_array.push(left_tables_array[i]); 
+    //console.log('temp_array');
+    //console.log(temp_array);
+    draw_table(ctx,temp_array,x,y,text_w,row_h);
+
+
+    // draw_table(ctx,left_tables_array,x,y,text_w,row_h);
 
     // parameters to draw right_table_array
-    x = w-10;
-    max_large_text = 0;
+    x = w-20;
+    max_large_text = 200;
+
+/*    
     text_w = 0;
     for ( var i=0; i < right_tables_array.length; i++ ){  
         if ( max_large_text < right_tables_array[i].length ) { max_large_text = right_tables_array[i]; } 
     }
-    text_w = ctx.measureText(max_large_text).width * 1.4;  
+    // text_w = ctx.measureText(max_large_text).width * 1.4; 
+*/     
+
+    text_w = max_large_text;
     console.log('draw_db, right text_w: ' + text_w);              
     x = x - text_w/2;             
-    y = 10;
-    draw_table(ctx,right_tables_array,x,y,text_w,row_h);
+    y = 50;
+
+    // Loop for differentiate distinct tables inside 'right_tables_array'
+    temp_array.length=0;
+    console.log('str: ' + right_tables_array[0] + ', substr posit: ' + right_tables_array[0].substr(right_tables_array[0].indexOf("- ")));
+    var temp_tbl_name = right_tables_array[0].substr(right_tables_array[0].indexOf("- ")+2,right_tables_array[0].length);
+    console.log('table name init ->' +  temp_tbl_name);
+    
+    for ( var i=0; i<right_tables_array.length; i++ ) {
+
+        console.log('step: ' + i);
+        console.log('table name -> ' + right_tables_array[i].substr(right_tables_array[i].indexOf("- ")+2,right_tables_array[0].length));
+        console.log('table name -> ' + temp_tbl_name);
+        
+        if ( temp_tbl_name == right_tables_array[i].substr(right_tables_array[i].indexOf("- ")+2,right_tables_array[0].length) ) { 
+            temp_array.push(right_tables_array[i]);            
+        }  
+        else { 
+            
+            console.log('temp_array');
+            console.log(temp_array);            
+            draw_table(ctx,temp_array,x,y,text_w,row_h);
+            y += 100;            
+            temp_array.length = 0;
+            temp_array.push(right_tables_array[i]); 
+            temp_tbl_name = right_tables_array[i].substr(right_tables_array[i].indexOf("- ")+2,right_tables_array[0].length);
+
+        }    
+
+    }
+    temp_array.push(right_tables_array[i]); 
+    console.log('temp_array');
+    console.log(temp_array);
+    draw_table(ctx,temp_array,x,y,text_w,row_h);
+
+    // draw_table(ctx,right_tables_array,x,y,text_w,row_h);
 
 }
