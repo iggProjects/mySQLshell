3782de23b39d57c9c4a5db8007f8bea1e58d2820
InakiGainza <igg.git.h@gmail.com>
2022-08-19 17:58:48 +0200

    commit "update config.servers.php"

--- config/config.servers.php
+++ config/config.servers.php
@@ -10,15 +10,15 @@
 $i_serv = 0;
 
 /*
- * First server
+ * First server: areafor DB server -> 'POAPMYSQL119.dns-servicio.com:3306'
 */
 
 $i_serv++;
 
 // Authentication type and info 
 $cfg_s['Servers'][$i_serv]['auth_type'] = 'config';
-$cfg_s['Servers'][$i_serv]['user'] = 'inaki2022';
-$cfg_s['Servers'][$i_serv]['password'] = 'Inaki@2022';
+$cfg_s['Servers'][$i_serv]['user'] = '';
+$cfg_s['Servers'][$i_serv]['password'] = '';
 $cfg_s['Servers'][$i_serv]['extension'] = 'mysqli';
 $cfg_s['Servers'][$i_serv]['AllowNoPassword'] = true;
 // $cfg_s['Lang'] = '';
@@ -31,7 +31,7 @@ $cfg_s['Servers'][$i_serv]['connect_type'] = 'tcp';
 // $dbcharset = 'utf8mb4';
 
 /*
- * Second server
+ * Second server: Localhost
 */
 
 $i_serv++;
@@ -40,7 +40,7 @@ $i_serv++;
 $cfg_s['Servers'][$i_serv]['auth_type'] = 'config';
 $cfg_s['Servers'][$i_serv]['user'] = 'root';
 //$cfg_s['Servers'][$i_serv]['password'] = '';
-$cfg_s['Servers'][$i_serv]['password'] = '@mysql@';
+$cfg_s['Servers'][$i_serv]['password'] = '';
 $cfg_s['Servers'][$i_serv]['extension'] = 'mysqli';
 $cfg_s['Servers'][$i_serv]['AllowNoPassword'] = false;
 // $cfg_s['Lang'] = '';
@@ -69,44 +69,5 @@ foreach ( $cfg_s as $server) {
 };
 
 $dbcharset = 'utf8mb4';
-
-
-
-/*
-
-***************************************
-        For every new server
-****************************************
-
-// User for advanced features 
-$cfg_s['Servers'][$i_serv]['controluser'] = 'pma';
-$cfg_s['Servers'][$i_serv]['controlpass'] = '';
-
-// Advanced phpMyAdmin features 
-$cfg_s['Servers'][$i_serv]['pmadb'] = 'phpmyadmin';
-$cfg_s['Servers'][$i_serv]['bookmarktable'] = 'pma__bookmark';
-$cfg_s['Servers'][$i_serv]['relation'] = 'pma__relation';
-$cfg_s['Servers'][$i_serv]['table_info'] = 'pma__table_info';
-$cfg_s['Servers'][$i_serv]['table_coords'] = 'pma__table_coords';
-$cfg_s['Servers'][$i_serv]['pdf_pages'] = 'pma__pdf_pages';
-$cfg_s['Servers'][$i_serv]['column_info'] = 'pma__column_info';
-$cfg_s['Servers'][$i_serv]['history'] = 'pma__history';
-$cfg_s['Servers'][$i_serv]['designer_coords'] = 'pma__designer_coords';
-$cfg_s['Servers'][$i_serv]['tracking'] = 'pma__tracking';
-$cfg_s['Servers'][$i_serv]['userconfig'] = 'pma__userconfig';
-$cfg_s['Servers'][$i_serv]['recent'] = 'pma__recent';
-$cfg_s['Servers'][$i_serv]['table_uiprefs'] = 'pma__table_uiprefs';
-$cfg_s['Servers'][$i_serv]['users'] = 'pma__users';
-$cfg_s['Servers'][$i_serv]['usergroups'] = 'pma__usergroups';
-$cfg_s['Servers'][$i_serv]['navigationhiding'] = 'pma__navigationhiding';
-$cfg_s['Servers'][$i_serv]['savedsearches'] = 'pma__savedsearches';
-$cfg_s['Servers'][$i_serv]['central_columns'] = 'pma__central_columns';
-$cfg_s['Servers'][$i_serv]['designer_settings'] = 'pma__designer_settings';
-$cfg_s['Servers'][$i_serv]['export_templates'] = 'pma__export_templates';
-$cfg_s['Servers'][$i_serv]['favorite'] = 'pma__favorite';
-
-// End of servers configuration
-
-*/
 
 ?>
