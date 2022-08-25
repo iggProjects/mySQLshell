1f00ef450d75c08cbf91a909b8681a323afd04f4
InakiGainza <igg.git.h@gmail.com>
2022-08-25 09:47:09 +0200

    commit ""

--- config/php_errors_config.php
+++ config/php_errors_config.php
@@ -3,6 +3,7 @@
 # 
 #  php errors to log file php_errors.log in asstes folder
 # 
+
 $php_errors_log = $_SERVER['DOCUMENT_ROOT'] . "/lanbide2022/gaztelu/curso-backend-areafor-server/assets/log-files/php_errors.log";
 error_reporting(E_ALL);
 ini_set('ignore_repeated_errors',TRUE);
