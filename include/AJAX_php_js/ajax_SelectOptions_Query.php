98ae60fdc54a85ab8dcfd97ae0b5c394e004b7de
InakiGainza <igg.git.h@gmail.com>
2022-09-03 12:38:33 +0200

    commit "ipdate ajax_SelectOptions case localhost"

--- include/AJAX_php_js/ajax_SelectOptions_Query.php
+++ include/AJAX_php_js/ajax_SelectOptions_Query.php
@@ -33,8 +33,8 @@ if ( $_REQUEST['sql_query'] ) {
 
 
 /* ONLY WORKING IN LOCALHOST IN 'my_lqs_admin' DB */
-$dbuser  = $cfg_s['Servers'][2]['user'];
-$dbpass = $cfg_s['Servers'][2]['password'];
+$dbuser  = $cfg_s['Servers'][1]['user'];
+$dbpass = $cfg_s['Servers'][1]['password'];
 $dbcharset = 'utf8mb4';
 
 # connection to DB using $db_parameters
